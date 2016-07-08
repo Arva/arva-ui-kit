6b34d1e13c2f2bedb83590004f57ef9a56fe62b2
Karl Lundfall <karl@bizboard.nl>
2016-07-08 16:32:20 +0200

    Fixed button to make a more awesome ripple

--- src/components/buttons/Button.js
+++ src/components/buttons/Button.js
@@ -11,6 +11,8 @@ import {combineOptions}     from 'arva-js/utils/CombineOptions.js';
 import {layout, options}    from 'arva-js/layout/decorators.js';
 import {Clickable}          from './Clickable.js'
 import {Ripple}             from './Ripple.js';
+import AnimationController  from 'famous-flex/AnimationController.js';
+
 
 @layout.translate(0, 0, 30)
 
@@ -18,6 +20,19 @@ export class Button extends Clickable {
     @layout.fullscreen
     @layout.translate(0, 0, -10)
     background = new Surface({properties: this.options.backgroundProperties});
+
+    @layout.animate({
+        showInitially: false,
+        animation: AnimationController.Animation.Fade,
+        transition:{duration: 220}
+    })
+    @layout.fullscreen
+    pressedIndication = new Surface({
+        properties: {
+            /*boxShadow: '0px 0px 15px rgba(0, 0, 0, 0.5) inset, 0px 0px 5px rgba(255, 255, 255, 0.5)',*/
+            backgroundColor: 'rgb(240, 240, 240)'
+        }
+    });
 
     @layout.place('bottom')
     @layout.translate(0, 0, -20)
@@ -66,15 +81,25 @@ export class Button extends Clickable {
 
     }
 
-    _doTapStart(mouseEvent) {
+    _doClick(mouseEvent) {
+        super._doClick(mouseEvent);
         if (this.ripple) {
             this.ripple.show(mouseEvent.offsetX, mouseEvent.offsetY);
         }
     }
 
+    _doTapStart() {
+        this.showRenderable('pressedIndication');
+    }
+
     _doTapEnd() {
-        if (this.ripple) {
-            this.ripple.hide();
+        /* Cancelling animation causes ugly hack:
+         * The AnimationController doesn't want to abort the animation without first showing the
+         * renderable, therefore we must hack the state a bit
+         */
+        if(this.renderables.pressedIndication.get()){
+            this.renderables.pressedIndication._viewStack[0].state = 1;
         }
+        this.hideRenderable('pressedIndication');
     }
 }
