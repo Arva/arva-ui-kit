6738fca3bd3d9b0c1f6c9b1d5e4f60c34a82d09f
Vlad Stan <vlad@bizboard.nl>
2016-07-25 17:23:33 +0200

    Refactored promises to async functions. Refactored local settings to the global DefaultSettings.js file.

--- src/utils/authentication/FacebookLogin.js
+++ src/utils/authentication/FacebookLogin.js
@@ -2,21 +2,17 @@
  * Created by tom on 26/05/15.
  */
 
-import {inject}                 from 'di';
+import {inject}                 from 'arva-js/utils/di/Decorators.js';
 import {DataSource}             from 'arva-js/data/DataSource.js';
-import facebookBrowserModule    from '../../../plugins/cordova-plugin-facebook4/www/facebook-browser.js';
 
-import {BaseLogin}              from './BaseLogin';
-import {FacebookAppID}          from '../../settings';
+import {BaseLogin}              from './BaseLogin.js';
+import {Settings}               from '../../defaults/DefaultSettings.js';
 
 /**
  * To get this login provider working, perform the following actions:
- * - install the cordova plugin com.phonegap.plugins.facebookconnect
- * - export FacebookAppID in /src/settings.js
- * - copy dummy /src/cordova.js and /src/exec.js files
+ * - install the cordova plugin --save cordova-plugin-facebook4
+ * - export facebookAppId in /arva-js/defaults/defaultSettings.js
  * - add a div with id "fb-root" to index.html
- * - add this to config.js' paths field: "plugins/*": "plugins/*.js"
- * - add this to config.js' maps field: "cordova": ""
  * - add the following to the whitelisted domains in index.html's Content-Security-Policy:
  *   https://auth.firebase.com http://connect.facebook.net https://connect.facebook.net http://*.ak.facebook.com https://*.ak.facebook.com
  */
@@ -31,62 +27,47 @@ export class FacebookLogin extends BaseLogin {
         super();
         this._dataSource = dataSource;
 
-        if(!window.facebookConnectPlugin) { window.facebookConnectPlugin = facebookBrowserModule; }
+        if (!Settings.facebookBrowserModule) {
+            throw new Error('You need to both install the Cordova ' +
+                'Facebook Plugin, and set facebookBrowserModule to the path to the browser.js of the locally installed ' +
+                'facebook plugin in DefaultSettings.js.\n\n' +
+                'For example: import facebook from \'../plugins/cordova-plugin-facebook4/www/facebook-browser.js\'; ' +
+                'Settings.facebookBrowserModule = facebook;');
+        }
+
+        if (!Settings.facebookAppId) {
+            throw new Error('You need to specify facebookAppID in DefaultSettings.js');
+        }
     }
 
-    setFacebookAppID(id = null){
-        this.facebookAppID = id;
+    async authenticate() {
+        /* If we're running in a browser, the plugin needs special initialization */
+        if (window.cordova && window.cordova.platformId === 'browser') {
+            Settings.facebookBrowserModule.browserInit(Settings.facebookAppID);
+        }
+        return await new Promise((resolve, reject) =>
+            Settings.facebookBrowserModule.login(['public_profile', 'email'], resolve, reject));
     }
 
-    authenticate() {
-
-        return new Promise((resolve, reject) => {
-            if (!this.facebookAppID) {
-                reject(new Error('No Facebook app ID is set, please set one using setFacebookAppID'));
-            }
-
-            /* If we're running in a browser, the plugin needs special initialization */
-            if (window.cordova && window.cordova.platformId === 'browser') {
-                facebookConnectPlugin.browserInit(this.facebookAppID);
-            }
-
-            facebookConnectPlugin.login(['public_profile', 'email'], resolve, reject);
-        });
+    async authenticateToDataSource() {
+        await this.authenticate();
+        let token = await new Promise((resolve) => Settings.facebookBrowserModule.getAccessToken(resolve));
+        if (token) {
+            let authData = await this._dataSource.authWithOAuthToken('facebook', token);
+            return {uid: authData.uid, profile: authData.facebook, provider: 'facebook'};
+        }
     }
 
-    authenticateToDataSource() {
-
-        return new Promise(async function (resolve, reject) {
-            try {
-                let data = await this.authenticate();
-                facebookConnectPlugin.getAccessToken((token) => {
-                    if (token) {
-                        this._dataSource.authWithOAuthToken('facebook', token, (error, authData) => {
-                            if (!error) {
-                                resolve({uid: authData.uid, profile: authData.facebook, provider: 'facebook'});
-                            } else {
-                                reject(error);
-                            }
-                        });
-                    }
-                });
-            } catch (error) {
-                reject(error);
-            }
-        }.bind(this));
-
-    }
-
-    deauthenticate() {
-        return new Promise((resolve, reject) => {
-            facebookConnectPlugin.logout(resolve, reject);
-        });
+    async deauthenticate() {
+        return await new Promise((resolve, reject) => Settings.facebookBrowserModule.logout(resolve, reject));
     }
 
     deauthenticateFromDataSource() {
         this._dataSource.unauth();
         try {
             this.deauthenticate();
-        } catch (error) { console.log('Error deauthenticating Facebook', error); }
+        } catch (error) {
+            console.log('Error deauthenticating Facebook', error);
+        }
     }
 }
