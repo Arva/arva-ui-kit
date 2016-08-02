dfc1bbdf5b1d70be515d6f6083c4b5c3dcc744d3
Karl Lundfall <karl@bizboard.nl>
2016-08-02 13:08:57 +0200

    Added function to change color of button

--- src/buttons/ImageButton.js
+++ src/buttons/ImageButton.js
@@ -40,4 +40,8 @@ export class ImageButton extends Button {
     setContent(iconConstructor) {
         this.image.setContent(new iconConstructor({color: this.options.properties.color}).getContent());
     }
+
+    changeColor() {
+        return this.image.changeColor(...arguments);
+    }
 }
