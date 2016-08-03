eeb359aecd1b036a60d8f226652030d5f76b472b
Karl Lundfall <karl@bizboard.nl>
2016-08-03 11:17:49 +0200

    Refactored colors to avoid using the colors directly which doesn't work because the references get replaced

--- src/uibars/UIBar.js
+++ src/uibars/UIBar.js
@@ -7,10 +7,12 @@ import Surface                                      from 'famous/core/Surface.js
 import {View}                                       from 'arva-js/core/View.js';
 import {layout}                                     from 'arva-js/layout/Decorators.js';
 import {combineOptions}                             from 'arva-js/utils/CombineOptions.js';
-import {PrimaryUIColor}                             from '../defaults/DefaultColors.js';
-import {UIBarHeight,
-        UIBarThickHeight,
-        UIBarPadding}                               from '../defaults/DefaultDimensions.js';
+import {Colors}                                     from '../defaults/DefaultColors.js';
+import {
+    UIBarHeight,
+    UIBarThickHeight,
+    UIBarPadding
+}                               from '../defaults/DefaultDimensions.js';
 
 const rgbToRgba = (rgbString, alpha) => {
     let color = new RGBColor(rgbString);
@@ -20,22 +22,39 @@ const rgbToRgba = (rgbString, alpha) => {
 
 @layout.dockPadding(0, UIBarPadding)
 export class UIBar extends View {
-    static backgroundSettings = {
-        'white': {
-            shadows: {
-                'softShadow': {boxShadow: '0px 0px 8px 0px rgba(0,0,0,0.12)'},
-                'hardShadow': {boxShadow: '0px 2px 0px 0px rgba(0,0,0,0.12)'}
-            },
-            backgroundColor: {backgroundColor: 'rgb(255, 255, 255)'}
-        },
-        'colored': {
-            shadows: {
-                'softShadow': {boxShadow: `0px 0px 8px 0px rgba(0,0,0,0.12), 0px 0px 8px 0px ${rgbToRgba(PrimaryUIColor, 0.12)}`},
-                'hardShadow': {boxShadow: `0px 2px 0px 0px rgba(0,0,0,0.08), 0px 2px 0px 0px ${rgbToRgba(PrimaryUIColor, 0.08)}`}
-            },
-            backgroundColor: {backgroundColor: PrimaryUIColor}
+    /**
+     * Gets the settings depending on the variation
+     * @param variation
+     * @returns {*}
+     */
+    static getSettings(variation) {
+        variation = variation || 'white';
+        switch (variation) {
+            default:
+                console.log('Invalid variation selected. Falling back to default settings (white).');
+            case 'white':
+                return {
+                    backgroundSettings: {
+                        shadows: {
+                            'softShadow': {boxShadow: '0px 0px 8px 0px rgba(0,0,0,0.12)'},
+                            'hardShadow': {boxShadow: '0px 2px 0px 0px rgba(0,0,0,0.12)'}
+                        },
+                        backgroundColor: {backgroundColor: 'rgb(255, 255, 255)'}
+                    }, opposingColor: Colors.PrimaryUIColor
+                };
+            case 'colored':
+                return {
+                    backgroundSettings: {
+                        shadows: {
+                            'softShadow': {boxShadow: `0px 0px 8px 0px rgba(0,0,0,0.12), 0px 0px 8px 0px ${rgbToRgba(Colors.PrimaryUIColor, 0.12)}`},
+                            'hardShadow': {boxShadow: `0px 2px 0px 0px rgba(0,0,0,0.08), 0px 2px 0px 0px ${rgbToRgba(Colors.PrimaryUIColor, 0.08)}`}
+                        },
+                        backgroundColor: {backgroundColor: Colors.PrimaryUIColor}
+                    },
+                    opposingColor: 'rgb(255, 255, 255)'
+                };
         }
-    };
+    }
 
     @layout.size(undefined, undefined)
     @layout.translate(0, 0, -10)
@@ -74,8 +93,8 @@ export class UIBar extends View {
 
         let components = options.components;
         for (let [renderable, renderableName, position] of components) {
-            if(this.options.autoColoring){
-                if(renderable.setColor) {
+            if (this.options.autoColoring) {
+                if (renderable.setColor) {
                     renderable.setColor(opposingColor);
                 } else if (renderable.setProperties) {
                     renderable.setProperties({color: opposingColor});
@@ -98,18 +117,7 @@ export class UIBar extends View {
          * Selects which variation settings the UIBar should use.
          * variation option can be set to 'white' [default] or 'colored'.
          */
-        let settings;
-        let opposingColor;
-        if (options.variation in UIBar.backgroundSettings) {
-            settings = UIBar.backgroundSettings[options.variation];
-            opposingColor = options.variation === 'colored' ?  'rgb(255, 255, 255)' : PrimaryUIColor;
-        } else {
-            settings = UIBar.backgroundSettings.white;
-            opposingColor = PrimaryUIColor;
-            if (options.variation) {
-                console.log('Invalid variation selected. Falling back to default settings (white).');
-            }
-        }
+        let {backgroundSettings, opposingColor} = UIBar.getSettings(options.variation);
 
         /**
          * Selects which type of shadow the UIBar should have.
@@ -117,8 +125,8 @@ export class UIBar extends View {
          * By default no shadows are added.
          */
         let shadow;
-        if (options.shadowType in settings.shadows) {
-            shadow = settings.shadows[options.shadowType];
+        if (options.shadowType in backgroundSettings.shadows) {
+            shadow = backgroundSettings.shadows[options.shadowType];
         } else {
             shadow = {};
             if (options.shadowType) {
@@ -134,6 +142,9 @@ export class UIBar extends View {
         let topLine = options.topLine ? {borderTop: '1px solid rgba(0, 0, 0, 0.1)'} : {};
         let bottomLine = options.bottomLine ? {borderBottom: '1px solid rgba(0, 0, 0, 0.1)'} : {};
 
-        return {backgroundProperties: {...shadow, ...topLine, ...bottomLine, ...settings.backgroundColor}, opposingColor};
+        return {
+            backgroundProperties: {...shadow, ...topLine, ...bottomLine, ...backgroundSettings.backgroundColor},
+            opposingColor
+        };
     }
 }
