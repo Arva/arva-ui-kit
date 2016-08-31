8a7ad2958b819c301ad57a6ff6382dc306b2fc3d
Karl Lundfall <karl@bizboard.nl>
2016-08-31 17:03:36 +0200

    Changed the default color for icons

--- src/icons/views/BaseIcon.js
+++ src/icons/views/BaseIcon.js
@@ -3,13 +3,13 @@
  */
 
 import Surface                          from 'famous/core/Surface.js';
-import {IconColor}                      from '../../defaults/DefaultColors.js';
+import {Colors}                         from '../../defaults/DefaultColors.js';
 import {replaceColors}                  from './ReplaceColors.js';
 
 export class BaseIcon extends Surface {
     constructor(options = {}){
         super({
-           content: replaceColors(options.icon, options.color || IconColor),
+           content: replaceColors(options.icon, options.color || Colors.PrimaryUIColor),
             ...options
         });
         this.options = options;
