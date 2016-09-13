3218bde05e9d08dd56a98103970ba896169dbad2
Karl Lundfall <karl@bizboard.nl>
2016-09-13 15:46:26 +0200

    Added auto sizing to multilineinputview

--- src/input/MultiLineInputView.js
+++ src/input/MultiLineInputView.js
@@ -8,7 +8,6 @@ import {layout}                     from 'arva-js/layout/decorators.js';
 import {combineOptions}             from 'arva-js/utils/CombineOptions.js';
 import {replaceEmojiAtEnd}          from './Emoji.js';
 
-
 import AutosizeTextareaSurface      from 'famous-autosizetextarea/AutosizeTextareaSurface.js';
 
 
@@ -18,30 +17,47 @@ export class MultiLineInputView extends View {
     input = new AutosizeTextareaSurface(combineOptions(this.options, {
         properties: {
             resize: 'none',
-            outline: 'none'
+            outline: 'none',
+            overflow: 'hidden'
         }
     }));
 
     constructor(options = {}) {
         super(combineOptions({
-            initialHeight: 50, clearOnEnter: true, emojiEnabled: true,
+            initialHeight: 50,
+            clearOnEnter: false,
+            emojiEnabled: true,
             placeholder: 'Type a message',
-            type: 'text', maxHeight: 150
+            type: 'text', 
+            maxHeight: 150,
+            properties: {
+                padding: '5px 10px 5px 10px'
+            }
         }, options));
 
         /* Set properties specificly as resize property is not set on construction */
-        this.input.setProperties({resize: 'none', padding: '5px 10px 5px 10px'});
+        this.input.setProperties({resize: 'none', padding: this.options.properties.padding});
+        this.input.on('scroll', () => this.input._element.scrollTop = 0);
 
         this.input.on('scrollHeightChanged', (scrollHeight)=> {
             if (scrollHeight < this.options.initialHeight) scrollHeight = this.options.initialHeight;
-            this._eventOutput.emit('changeInputHeight', scrollHeight > this.options.maxHeight ? this.options.maxHeight : scrollHeight);
+            this.reflowRecursively();
+            this._savedHeight = scrollHeight > this.options.maxHeight ? this.options.maxHeight : scrollHeight;
+            if(this._savedHeight === this.options.maxHeight){
+                this.input.setProperties({ overflow: 'scroll'});
+            } else {
+                this.input.setProperties({ overflow: 'hidden'});
+            }
         });
 
         this.input.on('keyup', this._onKeyUp);
         this.input.on('paste', this._onFieldChange);
         this.input.on('input', this._onFieldChange);
         this.input.on('propertychange', this._onFieldChange);
+    }
 
+    getSize() {
+        return [undefined, this._savedHeight || this.options.initialHeight]
     }
 
     _onKeyUp(event) {
@@ -54,7 +70,7 @@ export class MultiLineInputView extends View {
 
     _onMessageComplete() {
         let message = this.getValue();
-        if(!message) return;
+        if (!message) return;
 
         if (this.options.clearOnEnter) {
             this.input.setValue('');
@@ -66,7 +82,7 @@ export class MultiLineInputView extends View {
     _onFieldChange() {
         let currentValue = this.getValue();
         if (currentValue != this.input._value) {
-            if(this.options.emojiEnabled) {
+            if (this.options.emojiEnabled && typeof currentValue === 'string') {
                 currentValue = replaceEmojiAtEnd(currentValue);
                 this.setValue(currentValue);
             }
@@ -78,12 +94,12 @@ export class MultiLineInputView extends View {
         }
     }
 
-    setValue(){
+    setValue() {
         this.input.setValue(...arguments);
     }
 
-    getValue(){
-        let message =  this.input.getValue();
+    getValue() {
+        let message = this.input.getValue();
         message = message.trim();
         if (message === '' || message === undefined || message === "\r\n" || message === "\n" || message === "\r") {
             return false;
