91d9041ec4d2e935320cbde336e396976102ac74
Karl Lundfall <karl@bizboard.nl>
2016-11-11 15:17:17 +0100

    Fixed FacebookAuthentcation to work better

--- src/utils/authentication/FacebookLogin.js
+++ src/utils/authentication/FacebookLogin.js
@@ -10,44 +10,34 @@ import {Settings}               from '../../defaults/DefaultSettings.js';
 
 /**
  * To get this login provider working, perform the following actions:
- * - install the cordova plugin --save cordova-plugin-facebook4
- * - export facebookAppId in /arva-js/defaults/defaultSettings.js
- * - add a div with id "fb-root" to index.html
+ * - install the  cordova plugins add cordova-plugin-facebook4 --save --variable APP_ID="xxx" --variable APP_NAME="xxx"
  * - add the following to the whitelisted domains in index.html's Content-Security-Policy:
- *   https://auth.firebase.com http://connect.facebook.net https://connect.facebook.net http://*.ak.facebook.com https://*.ak.facebook.com
+ * https://auth.firebase.com http://connect.facebook.net https://connect.facebook.net http://*.ak.facebook.com https://*.ak.facebook.com
  */
 @inject(DataSource)
 export class FacebookLogin extends BaseLogin {
-
+    
     
     constructor(dataSource) {
-        super();
-
-        if (!Settings.facebookBrowserModule) {
-            throw new Error('You need to both install the Cordova ' +
-                'Facebook Plugin, and set facebookBrowserModule to the path to the browser.js of the locally installed ' +
-                'facebook plugin in DefaultSettings.js.\n\n' +
-                'For example: import facebook from \'../plugins/cordova-plugin-facebook4/www/facebook-browser.js\'; ' +
-                'Settings.facebookBrowserModule = facebook;');
-        }
-
-        if (!Settings.facebookAppId) {
-            throw new Error('You need to specify facebookAppID in DefaultSettings.js');
+        super(dataSource);
+        if(!window.facebookConnectPlugin){
+            console.log("Error - the facebook connect plugin 'cordova-plugin-facebook4' isn't properly installed")
         }
     }
 
-    async authenticate() {
-        /* If we're running in a browser, the plugin needs special initialization */
-        if (window.cordova && window.cordova.platformId === 'browser') {
-            Settings.facebookBrowserModule.browserInit(Settings.facebookAppID);
-        }
-        return await new Promise((resolve, reject) =>
-            Settings.facebookBrowserModule.login(['public_profile', 'email'], resolve, reject));
+    authenticate() {
+        return new Promise((resolve)=> {
+            facebookConnectPlugin.login(['public_profile', 'email'], (data) => {
+                resolve(data);
+            }, (reason) => {
+                reject(reason);
+            });
+        });
     }
 
     async authenticateToDataSource() {
         await this.authenticate();
-        let token = await new Promise((resolve) => Settings.facebookBrowserModule.getAccessToken(resolve));
+        let token = await new Promise((resolve) => facebookConnectPlugin.getAccessToken(resolve));
         if (token) {
             let authData = await this._dataSource.authWithOAuthToken('facebook', token);
             return {uid: authData.uid, profile: authData.facebook, provider: 'facebook'};
