4a131e6d9178ec26356be049646db502097ceb9f
Vlad Stan <vlad@bizboard.nl>
2017-01-30 14:35:12 +0100

    Added KeyValueList component and made some reformatting in DefaultTypefaces.

---
+++ src/list/KeyValueElement.js
@@ -0,0 +1,42 @@
+/**
+ * Created by vlad on 27/01/2017.
+ */
+
+import {Text}               from 'arva-kit/text/Text.js';
+import {View}               from 'arva-js/core/View.js';
+import {layout}             from 'arva-js/layout/Decorators.js';
+import {combineOptions}     from 'arva-js/utils/CombineOptions.js';
+
+export class KeyValueElement extends View {
+
+    @layout.dock.left()
+    @layout.stick.center()
+    @layout.size(~100, ~14)
+    key = new Text({
+        content: this.options.key,
+        properties: this.options.properties.key || {}
+    });
+
+    @layout.dock.right()
+    @layout.stick.center()
+    @layout.size(~100, ~14)
+    value = new Text({
+        content: this.options.value,
+        properties: this.options.properties.value || {}
+    });
+
+    constructor(options = {}) {
+        super(combineOptions({
+            properties: {}
+        }, options));
+    }
+
+    setKey(key) {
+        this.key.setContent(key);
+    }
+
+    setValue(value) {
+        this.value.setContent(value);
+    }
+
+}
---
+++ src/list/KeyValueList.js
@@ -0,0 +1,93 @@
+/**
+ * Created by vlad on 27/01/2017.
+ */
+
+import Surface              from 'famous/core/Surface.js';
+import {KeyValueElement}    from 'arva-kit/list/KeyValueElement.js';
+import {View}               from 'arva-js/core/View.js';
+import {layout}             from 'arva-js/layout/Decorators.js';
+import {combineOptions}     from 'arva-js/utils/CombineOptions.js';
+import {TypeFaces}          from 'arva-kit/defaults/DefaultTypefaces.js';
+
+export class KeyValueList extends View {
+
+    @layout.fullSize()
+    background = new Surface({
+        properties: {
+            backgroundColor: this.options.backgroundColor
+        }
+    });
+
+    /**
+     * A list of key value text elements
+     *
+     * @example
+     * keyValueList = new KeyValueList({
+     *     list: [
+     *         {key: 'Monday', value: '11:00 - 17:00'},
+     *         {key: 'Tuesday', value: '09:00 - 17:00'},
+     *         {key: 'Wednesday', value: '10:00 - 13:00'},
+     *         {key: 'Thursday', value: '09:00 - 21:00', properties: {color: 'rgb(255, 140, 42)'}},
+     *         {key: 'Friday', value: '09:00 - 17:00'},
+     *         {key: 'Saturday', value: '09:00 - 13:00'},
+     *         {key: 'Sunday', value: 'Closed'},
+     *     ],
+     *     textProperties: {
+     *         key: TypeFaces.UISmallGray,
+     *         value: TypeFaces.UISmall
+     *     },
+     *     backgroundColor: 'rgba(255, 100, 255, 1)',
+     *     spacing: 16
+     * });
+     *
+     * @param {Object} options Construction options
+     * @param {Array} [options.list] A list of objects containing each row's key and value. Each one of these object
+     *        can also contain properties to be applied on that specific element's value text.
+     * @param {Object} [options.textProperties] Contains the global custom key and value text properties.
+     * @param {String} [options.backgroundColor] Sets the background color of the KeyValueList
+     * @param {Number} [options.spacing] Sets a custom spacing between elements
+     */
+    constructor(options = {}) {
+        super(combineOptions({
+            list: [],
+            textProperties: {
+                key: TypeFaces.UISmallGray,
+                value: TypeFaces.UISmall
+            },
+            spacing: 16
+        }, options));
+
+        let list = this.options.list;
+        for (let i = 0; i < list.length; i++) {
+            let element = list[i];
+            this.addRenderable(
+                new KeyValueElement({
+                    key: element.key,
+                    value: element.value,
+                    properties: combineOptions(this.options.textProperties, {value: element.properties})
+                }), `element ${i}`,
+                layout.dock.top(~20, this.options.spacing)
+            );
+        }
+    }
+
+    setKeyValue(elementIndex, key, value) {
+        this.setKey(elementIndex, key);
+        this.setValue(elementIndex, value);
+    }
+
+    setKey(elementIndex, key) {
+        let element = this[`element ${elementIndex}`];
+        if (element) {
+            element.setKey(key);
+        }
+    }
+
+    setValue(elementIndex, value) {
+        let element = this[`element ${elementIndex}`];
+        if (element) {
+            element.setValue(value);
+        }
+    }
+
+}
