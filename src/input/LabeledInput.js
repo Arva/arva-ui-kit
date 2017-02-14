5f62b52d1c434db5dd79600a965e6f1040e6ec14
Karl Lundfall <karl@bizboard.nl>
2017-02-14 10:39:22 +0100

    Added small dock to LabeledInput

--- src/input/LabeledInput.js
+++ src/input/LabeledInput.js
@@ -8,6 +8,12 @@ import {combineOptions}         from 'arva-js/utils/CombineOptions.js';
 import {UISmallGray}            from '../text/UISmallGray.js';
 import {SingleLineTextInput}    from './SingleLineTextInput.js';
 
+/**
+ * A simple class for wrapping some kind of input with a label on top
+ *
+ * @param {String} options.label The text of the label
+ * @param {Renderable} options.input An (instanciated) renderable that is going to be displayed beneath the input
+ */
 export class LabeledInput extends View {
 
     @layout.dock.top(~20)
