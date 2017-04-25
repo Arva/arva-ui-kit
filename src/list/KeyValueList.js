6c7e3842b624045c29278b5037f99bce81e47a87
Tom <tom.clement@bizboard.nl>
2017-04-25 16:28:13 +0200

    Fixed KeyValueList not using correct typeface format

--- src/list/KeyValueList.js
+++ src/list/KeyValueList.js
@@ -2,11 +2,12 @@
  * Created by vlad on 27/01/2017.
  */
 
-import {KeyValueElement}            from 'arva-kit/list/KeyValueElement.js';
 import {View}                       from 'arva-js/core/View.js';
 import {layout}                     from 'arva-js/layout/Decorators.js';
 import {combineOptions}             from 'arva-js/utils/CombineOptions.js';
-import {UISmallGray,UISmall}         from 'arva-kit/defaults/DefaultTypefaces.js';
+
+import {KeyValueElement}            from './KeyValueElement.js';
+import {TypeFaces}                  from '../defaults/DefaultTypefaces.js';
 
 export class KeyValueList extends View {
 
@@ -43,8 +44,8 @@ export class KeyValueList extends View {
         super(combineOptions({
             list: [],
             textProperties: {
-                key: UISmallGray,
-                value: UISmall
+                key: TypeFaces.UISmallGray,
+                value: TypeFaces.UISmall
             },
             spacing: 16,
             keyColumnWidth: 100,
