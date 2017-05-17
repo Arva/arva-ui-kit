7d2dcf916fd048f080e01a34622064b0b23272fd
Manuel Overdijk <manuel.overdijk@gmail.com>
2017-05-17 11:36:57 +0200

    Tagged arva-js, famous and famous-flex to the latest releases

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ System.config({
   },
 
   map: {
-    "arva-js": "github:bizboard/arva-js@develop",
+    "arva-js": "github:bizboard/arva-js@1.2.5",
     "babel": "npm:babel-core@5.8.38",
     "babel-runtime": "npm:babel-runtime@5.8.38",
     "bowser": "npm:bowser@1.4.3",
@@ -23,35 +23,35 @@ System.config({
     "core-js": "npm:core-js@1.2.7",
     "css": "github:systemjs/plugin-css@0.1.25",
     "degrees-radians": "npm:degrees-radians@1.0.3",
-    "famous": "github:bizboard/famous@develop",
+    "famous": "github:bizboard/famous@0.3.12",
     "famous-autosizetextarea": "github:ijzerenhein/famous-autosizetextarea@0.3.1",
     "famous-bkimagesurface": "github:bizboard/famous-bkimagesurface@master",
-    "famous-flex": "github:bizboard/famous-flex@master",
-    "lodash": "npm:lodash@4.14.0",
+    "famous-flex": "github:bizboard/famous-flex@1.3.13",
+    "lodash": "npm:lodash@4.17.4",
     "rgbcolor": "npm:rgbcolor@0.0.4",
     "text": "github:systemjs/plugin-text@0.0.8",
-    "github:bizboard/arva-js@develop": {
+    "github:bizboard/arva-js@1.2.5": {
       "bowser": "npm:bowser@1.4.3",
       "camelcase": "npm:camelcase@2.1.1",
       "eventemitter3": "npm:eventemitter3@1.2.0",
-      "famous": "github:bizboard/famous@0.3.7",
-      "famous-flex": "github:bizboard/famous-flex@master",
+      "famous": "github:bizboard/famous@0.3.12",
+      "famous-flex": "github:bizboard/famous-flex@1.3.13",
       "fastclick": "npm:fastclick@1.0.6",
-      "firebase": "github:firebase/firebase-bower@3.2.1",
-      "lodash": "npm:lodash@4.14.0",
+      "firebase": "github:firebase/firebase-bower@3.9.0",
+      "lodash": "npm:lodash@4.17.4",
       "lodash-decorators": "npm:lodash-decorators@3.0.2",
       "ordered-hashmap": "npm:ordered-hashmap@1.0.0",
       "request-animation-frame-mock": "github:erykpiast/request-animation-frame-mock@0.1.8",
       "xml2js": "npm:xml2js@0.4.17"
     },
-    "github:bizboard/famous-flex@master": {
+    "github:bizboard/famous-flex@1.3.13": {
       "es6-map": "npm:es6-map@0.1.5"
     },
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.4.1"
     },
-    "github:jspm/nodelibs-buffer@0.1.0": {
-      "buffer": "npm:buffer@3.6.0"
+    "github:jspm/nodelibs-buffer@0.1.1": {
+      "buffer": "npm:buffer@5.0.6"
     },
     "github:jspm/nodelibs-events@0.1.1": {
       "events": "npm:events@1.0.2"
@@ -60,7 +60,7 @@ System.config({
       "path-browserify": "npm:path-browserify@0.0.0"
     },
     "github:jspm/nodelibs-process@0.1.2": {
-      "process": "npm:process@0.11.9"
+      "process": "npm:process@0.11.10"
     },
     "github:jspm/nodelibs-stream@0.1.0": {
       "stream-browserify": "npm:stream-browserify@1.0.0"
@@ -79,23 +79,19 @@ System.config({
     },
     "npm:assert@1.4.1": {
       "assert": "github:jspm/nodelibs-assert@0.1.0",
-      "buffer": "github:jspm/nodelibs-buffer@0.1.0",
+      "buffer": "github:jspm/nodelibs-buffer@0.1.1",
       "process": "github:jspm/nodelibs-process@0.1.2",
       "util": "npm:util@0.10.3"
     },
     "npm:babel-runtime@5.8.38": {
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
-    "npm:buffer@3.6.0": {
-      "base64-js": "npm:base64-js@0.0.8",
-      "child_process": "github:jspm/nodelibs-child_process@0.1.0",
-      "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "ieee754": "npm:ieee754@1.1.6",
-      "isarray": "npm:isarray@1.0.0",
-      "process": "github:jspm/nodelibs-process@0.1.2"
+    "npm:buffer@5.0.6": {
+      "base64-js": "npm:base64-js@1.2.0",
+      "ieee754": "npm:ieee754@1.1.8"
     },
     "npm:clone@1.0.2": {
-      "buffer": "github:jspm/nodelibs-buffer@0.1.0",
+      "buffer": "github:jspm/nodelibs-buffer@0.1.1",
       "systemjs-json": "github:systemjs/plugin-json@0.1.2",
       "vm": "github:jspm/nodelibs-vm@0.1.0"
     },
@@ -114,27 +110,27 @@ System.config({
       "systemjs-json": "github:systemjs/plugin-json@0.1.2"
     },
     "npm:core-util-is@1.0.2": {
-      "buffer": "github:jspm/nodelibs-buffer@0.1.0"
+      "buffer": "github:jspm/nodelibs-buffer@0.1.1"
     },
     "npm:d@1.0.0": {
-      "es5-ext": "npm:es5-ext@0.10.15"
+      "es5-ext": "npm:es5-ext@0.10.18"
     },
     "npm:degrees-radians@1.0.3": {
       "assert": "github:jspm/nodelibs-assert@0.1.0"
     },
-    "npm:es5-ext@0.10.15": {
+    "npm:es5-ext@0.10.18": {
       "es6-iterator": "npm:es6-iterator@2.0.1",
       "es6-symbol": "npm:es6-symbol@3.1.1",
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:es6-iterator@2.0.1": {
       "d": "npm:d@1.0.0",
-      "es5-ext": "npm:es5-ext@0.10.15",
+      "es5-ext": "npm:es5-ext@0.10.18",
       "es6-symbol": "npm:es6-symbol@3.1.1"
     },
     "npm:es6-map@0.1.5": {
       "d": "npm:d@1.0.0",
-      "es5-ext": "npm:es5-ext@0.10.15",
+      "es5-ext": "npm:es5-ext@0.10.18",
       "es6-iterator": "npm:es6-iterator@2.0.1",
       "es6-set": "npm:es6-set@0.1.5",
       "es6-symbol": "npm:es6-symbol@3.1.1",
@@ -142,45 +138,37 @@ System.config({
     },
     "npm:es6-set@0.1.5": {
       "d": "npm:d@1.0.0",
-      "es5-ext": "npm:es5-ext@0.10.15",
+      "es5-ext": "npm:es5-ext@0.10.18",
       "es6-iterator": "npm:es6-iterator@2.0.1",
       "es6-symbol": "npm:es6-symbol@3.1.1",
       "event-emitter": "npm:event-emitter@0.3.5"
     },
     "npm:es6-symbol@3.1.1": {
       "d": "npm:d@1.0.0",
-      "es5-ext": "npm:es5-ext@0.10.15"
+      "es5-ext": "npm:es5-ext@0.10.18"
     },
     "npm:event-emitter@0.3.5": {
       "d": "npm:d@1.0.0",
-      "es5-ext": "npm:es5-ext@0.10.15",
+      "es5-ext": "npm:es5-ext@0.10.18",
       "events": "github:jspm/nodelibs-events@0.1.1"
     },
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
     },
-    "npm:isarray@1.0.0": {
-      "systemjs-json": "github:systemjs/plugin-json@0.1.2"
-    },
     "npm:lodash-decorators@3.0.2": {
-      "lodash": "npm:lodash@4.14.0",
+      "lodash": "npm:lodash@4.17.4",
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:path-browserify@0.0.0": {
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
-    "npm:process@0.11.6": {
-      "assert": "github:jspm/nodelibs-assert@0.1.0",
-      "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "vm": "github:jspm/nodelibs-vm@0.1.0"
-    },
-    "npm:process@0.11.9": {
+    "npm:process@0.11.10": {
       "assert": "github:jspm/nodelibs-assert@0.1.0",
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "vm": "github:jspm/nodelibs-vm@0.1.0"
     },
     "npm:readable-stream@1.1.14": {
-      "buffer": "github:jspm/nodelibs-buffer@0.1.0",
+      "buffer": "github:jspm/nodelibs-buffer@0.1.1",
       "core-util-is": "npm:core-util-is@1.0.2",
       "events": "github:jspm/nodelibs-events@0.1.1",
       "inherits": "npm:inherits@2.0.1",
@@ -192,8 +180,8 @@ System.config({
     "npm:rgbcolor@0.0.4": {
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
-    "npm:sax@1.2.1": {
-      "buffer": "github:jspm/nodelibs-buffer@0.1.0",
+    "npm:sax@1.2.2": {
+      "buffer": "github:jspm/nodelibs-buffer@0.1.1",
       "process": "github:jspm/nodelibs-process@0.1.2",
       "stream": "github:jspm/nodelibs-stream@0.1.0",
       "string_decoder": "github:jspm/nodelibs-string_decoder@0.1.0"
@@ -204,15 +192,14 @@ System.config({
       "readable-stream": "npm:readable-stream@1.1.14"
     },
     "npm:string_decoder@0.10.31": {
-      "buffer": "github:jspm/nodelibs-buffer@0.1.0"
+      "buffer": "github:jspm/nodelibs-buffer@0.1.1"
     },
     "npm:timers-browserify@1.4.2": {
-      "process": "npm:process@0.11.6"
+      "process": "npm:process@0.11.10"
     },
     "npm:util@0.10.3": {
       "inherits": "npm:inherits@2.0.1",
-      "process": "github:jspm/nodelibs-process@0.1.2",
-      "systemjs-json": "github:systemjs/plugin-json@0.1.2"
+      "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:vm-browserify@0.0.4": {
       "indexof": "npm:indexof@0.0.1"
@@ -220,12 +207,12 @@ System.config({
     "npm:xml2js@0.4.17": {
       "events": "github:jspm/nodelibs-events@0.1.1",
       "process": "github:jspm/nodelibs-process@0.1.2",
-      "sax": "npm:sax@1.2.1",
+      "sax": "npm:sax@1.2.2",
       "timers": "github:jspm/nodelibs-timers@0.1.0",
       "xmlbuilder": "npm:xmlbuilder@4.2.1"
     },
     "npm:xmlbuilder@4.2.1": {
-      "lodash": "npm:lodash@4.14.0",
+      "lodash": "npm:lodash@4.17.4",
       "process": "github:jspm/nodelibs-process@0.1.2"
     }
   }
