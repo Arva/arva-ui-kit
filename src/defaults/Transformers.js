41e6ca7d875d25fed05c07da0d8ebe3c2d19d78d
Christopher Catton <chrwc@Christophers-Mac-mini.local>
2017-06-09 13:21:36 +0200

    HTMLTransformer should make text input a string if not already.

--- src/defaults/Transformers.js
+++ src/defaults/Transformers.js
@@ -3,5 +3,5 @@ import Encoder        from 'node-html-encoder';
 let HTMLEncoder = new Encoder.Encoder('entity');
 
 export let HTMLTextTransformer = (Text) => {
-    return HTMLEncoder.htmlEncode(Text);
+    return HTMLEncoder.htmlEncode(""+Text);
 };
