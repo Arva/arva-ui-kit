2eb3a3093b70fa22176982991e7af0b72ecbd026
Christopher Catton <chrwc@Christophers-Mac-mini.local>
2017-06-09 13:45:12 +0200

    Added a small comment. See the comment for more information.

--- src/defaults/Transformers.js
+++ src/defaults/Transformers.js
@@ -3,5 +3,6 @@ import Encoder        from 'node-html-encoder';
 let HTMLEncoder = new Encoder.Encoder('entity');
 
 export let HTMLTextTransformer = (Text) => {
+    /* Warning: HTMLEncoder will throw an error if (first) argument is not a string. */
     return HTMLEncoder.htmlEncode(""+Text);
 };
