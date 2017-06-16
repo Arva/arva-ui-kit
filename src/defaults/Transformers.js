610b5302c232731ce7159a1734d7afffea426f2f
saferthanhouses <joey_oliver@hotmail.com>
2017-06-16 13:01:14 +0200

    Replaced HTMLEncoder with string replace for <> characters

--- src/defaults/Transformers.js
+++ src/defaults/Transformers.js
@@ -1,7 +1,6 @@
-import Encoder        from 'node-html-encoder';
 
-let HTMLEncoder = new Encoder.Encoder('entity');
+let htmlRegexp = new RegExp(/[\<\>]+/g);
 
-export let HTMLTextTransformer = (Text) => {
-    return HTMLEncoder.htmlEncode(Text);
+export let HTMLTextTransformer = (transformable) => {
+    return (transformable + '').replace(htmlRegexp, '')
 };
