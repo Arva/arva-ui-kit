6289bf62da1c8e1cddc17942540b2095e657db83
Paul van Motman <paul@bizboard.nl>
2017-08-02 18:05:22 +0200

    Updated borderRadius option of contentCard

--- src/components/ContentCard.js
+++ src/components/ContentCard.js
@@ -17,8 +17,8 @@ export class ContentCard extends View {
             ...this.options.backgroundProperties,
             backgroundColor: this.options.backgroundColor,
             border: this.options.border,
-            borderRadius: '4px',
-            boxShadow: getShadow({type: this.options.shadow}),
+            borderRadius: this.options.borderRadius ? this.options.borderRadius : '4px',
+            boxShadow: this.options.boxShadow ? this.options.boxShadow : getShadow({type: this.options.shadow}),
             cursor: this.options.enabled ? 'pointer' : 'initial'
         }
     });
