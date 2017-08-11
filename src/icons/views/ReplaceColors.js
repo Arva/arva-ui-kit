70a6e829b96f1da2fccd124fc8f95d657c17749b
saferthanhouses <joesamoliver@gmail.com>
2017-08-11 15:06:25 +0200

    Update replaceColors to hit rgb/rgba/hex & stroke & fill

--- src/icons/views/ReplaceColors.js
+++ src/icons/views/ReplaceColors.js
@@ -2,25 +2,19 @@
  * Created by Manuel on 06/07/16.
  */
 
-/* Todo check what the use was of this function*/
-// export function replaceColors(svgImage = '', color = '#000000'){
-//     return `${svgImage.replace(/#000000/g, color)}`
-// }
 
-/* Todo check if we can get this working*/
-// export function replaceColors(svgImage = '', color = '#000000'){
-//     return `${svgImage.replace(/^#[0-9a-f]{3,6}$/g, color)}`
-// }
+/********
+ *
+ * JO:
+ *
+ * Some icons have their colors defined using fill, some with stroke, replace both
+ * Replace both hex codes and rgb / rgba values
+ *
+ */
+
+const colorMatcher = new RegExp('(stroke|fill)="(#[A-Fa-f0-9]{6}|rgb\(.+\)|rgba\(.+\))"', 'gi');
 
 export function replaceColors(svgImage = '', color = '#000000'){
-    /* We need to find the 'fill="#' part of the svg and replace the hex color, e.g. #000000,
-    therefore the required index is searchString.length - 1. */
-    let searchString = 'fill="#';
-    let hexCollorLength = 7;
-    let index = svgImage.indexOf(searchString) + searchString.length - 1;
-    let replaceAt = (string, index, replacement) => {
-        return string.substr(0, index) + replacement + string.substr(index + hexCollorLength);
-    };
-    let newSvgImage = replaceAt(svgImage, index, color);
-    return newSvgImage;
+    return svgImage.replace(colorMatcher, `$1="${color}"`);
 }
+
