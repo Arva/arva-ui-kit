c5fb055b290feafcb6b7fcb79d54d997e2487ed0
Paul van Motman <paul@bizboard.nl>
2017-09-07 17:06:44 +0200

    Fixed ColoredIconButton so that it’s possible to set up active and inactive colours

--- src/buttons/ColoredIconButton.js
+++ src/buttons/ColoredIconButton.js
@@ -9,6 +9,16 @@ import {ColoredTextButton}    from './ColoredTextButton.js';
 
 export class ColoredIconButton extends WhiteIconButton {
     constructor(options){
-        super(combineOptions(ColoredTextButton.generateOptions(options), options));
+        super(combineOptions(options.colorProperties ? {
+            iconProperties: {color: options.colorProperties.activeIconColor},
+            backgroundProperties: {
+                backgroundColor: options.colorProperties.activeBackgroundColor
+            }
+        } : ColoredTextButton.generateOptions(options), options));
+    }
+
+    _setEnabled(enabled) {
+        let properties = this.options.colorProperties ? this.options.colorProperties : null;
+        super._setEnabled(enabled, true, properties);
     }
 }
