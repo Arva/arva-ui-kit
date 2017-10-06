e317a424db1e2b4a5311447dbf4c061badf8d8c5
Karl Lundfall <kalle.lundfall@gmail.com>
2017-10-06 16:18:57 +0200

    Fixed whitespace and added missing parameter in Clickable

--- src/components/Clickable.js
+++ src/components/Clickable.js
@@ -1,17 +1,18 @@
 /**
  * Created by lundfall on 07/07/16.
  */
-import Timer                from 'famous/utilities/Timer.js';
+import Timer from 'famous/utilities/Timer.js';
 
-import {View}               from 'arva-js/core/View.js';
-import { layout, bindings, event, dynamic }             from 'arva-js/layout/Decorators.js'
+import {View} from 'arva-js/core/View.js';
+import {layout, bindings, event, dynamic} from 'arva-js/layout/Decorators.js'
 
 
 @bindings.setup({
-  enabled: true,
-  clickEventName: 'buttonClick',
-  easyPress: false,
-  disableAfterClick: false
+    enabled: true,
+    clickEventName: 'buttonClick',
+    easyPress: false,
+    disableAfterClick: false,
+    clickEventData: []
 })
 export class Clickable extends View {
 
@@ -30,17 +31,19 @@ export class Clickable extends View {
      *
      * @param {Object} options
      * @param {String}  [options.clickEventName] An event to fire after click has happened.
-     * @param {String}  [options.clickEventData] An array of data to fire after click has happened.
+     * @param {Array}   [options.clickEventData] An array of data to fire after click has happened.
      * @param {Boolean} [options.disableAfterClick] If set to true, disables the clickable after it's been clicked.
      * @param {Boolean} [options.enabled] If set to false, the clickable does not trigger clickEventName.
      * @param {Boolean} [options.easyPress] If set to true, triggers a click with the clickEventName, clickEventData,
      * and _handleClick on tap start instead of click
      *
      */
-    constructor(options ) {
+    constructor(options) {
         super(options);
         if (this.options.enabled === false) {
-            this.once('deploy', () => { this._setEnabled(this.options.enabled); })
+            this.once('deploy', () => {
+                this._setEnabled(this.options.enabled);
+            })
         }
         this._setupListeners();
     }
@@ -80,7 +83,7 @@ export class Clickable extends View {
         }
     }
 
-    _onTouchMove(){
+    _onTouchMove() {
         if (this.overlay && !this._isInBounds(event, this.overlay)) {
             this._tapActive = false;
             this._handleTapRemoved();
@@ -89,7 +92,7 @@ export class Clickable extends View {
     }
 
     _onTapEnd(mouseEvent) {
-        if(this._tapActive){
+        if (this._tapActive) {
             this._eventOutput.emit('tapEnd');
             this._tapActive = false;
             this._handleTapEnd(mouseEvent);
@@ -98,22 +101,22 @@ export class Clickable extends View {
 
     _onMouseOut(mouseEvent) {
         let overlay = this.overlay;
-        if(this._tapActive && (!overlay || mouseEvent.target === overlay._element)){
+        if (this._tapActive && (!overlay || mouseEvent.target === overlay._element)) {
             this._tapActive = false;
             this._handleTapRemoved(mouseEvent);
         }
         this._handleMouseOut();
     }
 
-    _handleTouchMove(event){
+    _handleTouchMove(event) {
         /* To be inherited */
     }
 
-    _handleTapRemoved(){
+    _handleTapRemoved() {
         /* To be inherited */
     }
 
-    _handleMouseOut(){
+    _handleMouseOut() {
         /* To be inherited */
     }
 
@@ -160,7 +163,7 @@ export class Clickable extends View {
     _handleClick(mouseEvent) {
         let {options} = this;
         let {delay} = options;
-        if(options.disableAfterClick){
+        if (options.disableAfterClick) {
             this.disable();
         }
         let emit = () => this._eventOutput.emit(options.clickEventName, ...(options.clickEventData || []));
@@ -197,7 +200,7 @@ export class Clickable extends View {
      * @private
      */
     _isInBounds(touch, element = {}) {
-        if(element._currentTarget) {
+        if (element._currentTarget) {
             let elementPosition = element._currentTarget.getBoundingClientRect();
             let {left, right, top, bottom} = elementPosition;
 
